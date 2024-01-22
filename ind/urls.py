621e8dffab214ccd9b1faa5fe2ac71d4cab83a25
harshkhandelwal97 <virendrakhandelwal78@gmail.com>
2024-01-22 17:16:06 +0530

    done

--- ind/urls.py
+++ ind/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
  
 urlpatterns =[
-    path('log/',views.log)
+    path('log/',views.log),
+    path('home/',views.home),
+    path('progress/',views.progress),
+    path('comments/',views.comment)
 ]
--- ind/views.py
+++ ind/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 # Create your views here.
 def log(request):
     return render(request,'login.html')
+def home(request):
+    return render(request,'home.html')
+def progress(request):
+    return render(request,'progress.html')
+def comment(request):
+    return render(request,'comment.html')
