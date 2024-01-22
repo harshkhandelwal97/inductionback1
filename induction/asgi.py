47c8651e7acb7cea55c2e73f8939cf1ed5685525
harshkhandelwal97 <virendrakhandelwal78@gmail.com>
2024-01-22 16:31:13 +0530

    made

---
+++ induction/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for induction project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'induction.settings')
+
+application = get_asgi_application()
