b580c846b2a674342f455bfdae01a3d91895708d
harshkhandelwal97 <virendrakhandelwal78@gmail.com>
2024-01-23 21:29:15 +0530

    added models.py

--- ind/models.py
+++ ind/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.user.username
+    
+class Post(models.Model):
+    date = models.DateField()
+    description = models.TextField(max_length=200)
+    photo1 = models.ImageField(upload_to='post_photos/', blank=True, null=True)
+    photo2 = models.ImageField(upload_to='post_photos/', blank=True, null=True)
+    photo3 = models.ImageField(upload_to='post_photos/', blank=True, null=True)
+    likes_received = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"Post {self.id}"
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    comment = models.TextField()
+
+    def __str__(self):
+        return f"Comment {self.id} by {self.user.username}"
